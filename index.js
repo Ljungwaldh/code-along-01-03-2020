c43c8dfdbc758216aa4bbee417fa1365adf2c031
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-01 08:43:31 +0000

    adding initial html and javascript to make search function

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function search () {
+
+    var user_input = document.getElementById("user_input").value;
+    //var query = $(#user_input).val() This is jQuery version of above
+    console.log(user_input);
+}
+
