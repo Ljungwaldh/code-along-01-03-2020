df01d319cbb032d22c39b597e57639c2cb77e745
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-01 09:02:10 +0000

    Added further comments and javascript

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 function search () {
 
+
+    // GETTING USER INPUT
     var user_input = document.getElementById("user_input").value;
     //var query = $(#user_input).val() This is jQuery version of above
     console.log(user_input);
+
+    // CALLING MY API
+    // Building the request object
+
+    var request = new XMLHttpRequest(); // ADMIT
+    
 }
 
