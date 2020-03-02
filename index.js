ad9589e4476cc00e3609a29a94899e6508c18320
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-02 05:44:25 +0000

    javascript and comments added around request object

--- index.js
+++ index.js
@@ -8,9 +8,28 @@ function search () {
     console.log(user_input);
 
     // CALLING MY API
-    // Building the request object
 
-    var request = new XMLHttpRequest(); // ADMIT
+    // Building the request object
+    // ADMIT THE FOLLOWING
+    var api_request = new XMLHttpRequest();
+    api_request.onreadystatechange = function() {
+        if (this.readystate == 4 && this-status == 200) {
+            
+            // function to be run upon receiving a response
+            displayResults(api_request.responseText);
+        } else (
+
+            // function to be called upon an error response
+            console.log("This stuff is not working");
+        )
+    };
+
+    // UNDERSTAND THE FOLLOWING
+    endpoint = // who (www.example.com) - what server
+    query = // what data do you want
+    the_url = // who (www.example.com) + what (query)
+    api_request.open ("GET", the_url, true); // ADMIT "GET" AND "true"
+    api_request.send()
     
 }
 
