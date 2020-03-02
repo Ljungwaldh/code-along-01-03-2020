1f00ac72d392deea4b4e02fc3df11d33125388cc
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-02 12:24:23 +0000

    FInal commit of walkthrough with Xavier on API's

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
+function displayDataInMyPage() {
+        console.log(data_as_an_object);
+        console.log(data_as_an_object.main.temp_min);
+        console.log(data_as_an_object.sys.country);
+
+        var description = document.getElementById("description");
+        description.innerHTML = data_as_an_object.weather[0].description;
+
+        var temp_min = document.getElementById("temp_min");
+        temp_min.innerHTML = data_as_an_object.main.temp_min;
+
+        var country = document.getElementById("country");
+        country.innerHTML = data_as_an_object.sys.country;
+
+}
+
 function search () {
 
 
@@ -13,22 +29,35 @@ function search () {
     // ADMIT THE FOLLOWING
     var api_request = new XMLHttpRequest();
     api_request.onreadystatechange = function() {
-        if (this.readystate == 4 && this-status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
             
             // function to be run upon receiving a response
-            displayResults(api_request.responseText);
-        } else (
+            the_response = api_request.responseText; // response = data
+            // displayResults(the_response);
+            console.log(typeof the_response);
+
+            // THE RESPONSE YOU GET BACK IS OF STRING TYPE
+            // WE NEED TO CONVERT IT TO AN OBJECT --> using JSON.parse()
+            // EXTRA NOTE - BE SURE TO ORGANIZE DATA FROM API PROPERLY SO YOU CAN REFER CORRECTLY
+            data_as_an_object = JSON.parse(the_response);
+            
+            displayDataInMyPage (data_as_an_object);
+
+        }
+        
+        else {
 
             // function to be called upon an error response
-            console.log("This stuff is not working");
-        )
+            console.log("This stuff is not working")
+    }
     };
 
     // UNDERSTAND THE FOLLOWING
-    endpoint = "api.openweathermap.org/data/2.5/weather?";         // who (www.example.com) - what server
-    query = "q={city name}";                                    // what data do you want
-    key = "&appid=7816d9f235c88adc096427a68ca872f2";         //key to identify yourself to the server : who is asking for data?
-    the_url = endpoint + query + key                        // ELEMENTS FOR ALL API CALLS (maybe not key if api is public)
+    var endpoint = "https://api.openweathermap.org/data/2.5/weather?";         // who (www.example.com) - what server
+    var query = "q=" + user_input;                                    // what data do you want
+    var key = "&appid=7816d9f235c88adc096427a68ca872f2";         //key to identify yourself to the server : who is asking for data?
+    var the_url = endpoint + query + key;                        // ELEMENTS FOR ALL API CALLS (maybe not key if api is public)
+    
     api_request.open ("GET", the_url, true); // ADMIT "GET" AND "true"
     api_request.send()
     
